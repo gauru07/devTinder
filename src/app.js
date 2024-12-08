3e72a563ac50fa18a99e8cd0f018a78089d77b15
gaurav07 <gauravmanik2002@gmail.com>
2024-12-08 15:24:21 +0530

    New Change post get

--- src/app.js
+++ src/app.js
@@ -3,16 +3,37 @@ const express = require('express');
 
 const app = express();
 
-app.use("/Hello" , (req, res) => {
-    res.send("hello hello hello");
+app.get("/user" , (req , res) => {
+    res.send({
+        "firstName" : "Gaurav",
+        "lastName"  : "Satpute",
+        "age" : "22"
+    });
 });
 
-app.use("/sys" , (req, res) => {
-    res.send("sys sys  hello hello");
+app.post("/user" , (req , res) => {
+   //data saved succesfully in data base
+   res.send("Data Saved in Data BAse");
 });
-app.use("/" , (req, res) => {
+
+app.delete("/user" , (req , res) => {
+    res.send("Deleted Sucessfully");
+});
+
+
+
+// app.use("/Hello" , (req, res) => {
+//     res.send("hello hello hello");
+// });
+
+// app.use("/sys" , (req, res) => {
+//     res.send("sys sys  hello hello");
+// });
+
+app.use("/test" , (req, res) => {
     res.send("hello from the server");
 });
+
 
 app.listen(3000 , () => {
     console.log("server is succefully listening on port 3000");
